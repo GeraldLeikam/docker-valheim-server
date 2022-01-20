87bba20a82ec8b4e10757fca7b44ad79ac8fce24
Gerald Leikam <gerald.leikam@aol.com>
2022-01-20 13:51:07 +0100

    commited by software

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,13 +7,26 @@ DEFAULT_WORLD_NAME="DefaultGeneratedWorld"
 DEFAULT_SERVER_PASSWORD=secret
 DEFAULT_SAVE_DIR=/valheim-server/save
 DEFAULT_PUBLIC=1
-DEFAULT_GAME_MODE=vanilla
 
 if [[ -n "${SERVER_NAME}" ]]; then SERVER_NAME="${SERVER_NAME}"; else SERVER_NAME=${DEFAULT_SERVER_NAME}; fi
-if [ -n "${SERVER_PORT}" ]; then SERVER_PORT=${SERVER_PORT}; else SERVER_PORT=${DEFAULT_SERVER_PORT} ; fi
-if [ -n "${WORLD_NAME}" ]; then WORLD_NAME="${WORLD_NAME}"; else WORLD_NAME="${DEFAULT_WORLD_NAME}"; fi
+if [[ -n "${SERVER_PORT}" ]]; then SERVER_PORT=${SERVER_PORT}; else SERVER_PORT=${DEFAULT_SERVER_PORT} ; fi
+if [[ -n "${WORLD_NAME}" ]]; then WORLD_NAME="${WORLD_NAME}"; else WORLD_NAME="${DEFAULT_WORLD_NAME}"; fi
 if [[ ${#SERVER_PASSWORD} -gt 4 ]]; then SERVER_PASSWORD="${SERVER_PASSWORD}"; else SERVER_PASSWORD="${DEFAULT_SERVER_PASSWORD}"; fi
-if [ -n "${GAME_MODE}" ]; then GAME_MODE="${GAME_MODE}"; else GAME_MODE=${DEFAULT_GAME_MODE}; fi
+if [ -n "${PUBLIC}" ];
+then
+  if [[ "$(echo $PUBLIC | tr A-Z a-z)" = "true" ]]
+  then
+    PUBLIC=1
+  elif [[ "$(echo $PUBLIC | tr A-Z a-z)" = "false" ]]
+  then
+    PUBLIC=0
+  else
+    PUBLIC=${DEFAULT_PUBLIC}
+  fi
+else
+  PUBLIC=${DEFAULT_PUBLIC}
+fi
+
 
 if [[ "${DEBUG}" == "true" ]]
 then
@@ -23,7 +36,5 @@ then
   echo "SERVER_PASSWORD -> ${SERVER_PASSWORD}"
   echo "SAVE_DIR -> ${DEFAULT_SAVE_DIR}"
   echo "PUBLIC -> ${PUBLIC}"
-  echo "GAME_MODE -> ${GAME_MODE}"
   echo "AUTOUPDATE -> ${SCRIPT_AUTOUPDATE}"
-  echo $ARGUMENT_STRING
 fi
