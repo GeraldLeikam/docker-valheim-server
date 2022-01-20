6534b356d538dfa890ed7dcaf6fab90c2af157e2
Gerald Leikam <gerald.leikam@aol.com>
2022-01-20 23:08:35 +0100

    commited by software

--- scripts/logfilter.sh
+++ scripts/logfilter.sh
@@ -9,12 +9,7 @@ do
   for word in $misfits_words
   do
     echo $word
-    if [[ $line =~ $word ]]
-    then
-       echo "It's there!"
-    else
-      echo "It's not there"
-    fi
+
 
   done
   echo $line
