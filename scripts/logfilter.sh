a521c38cb8767a052485e8706a9c0f63d406770f
Gerald Leikam <gerald.leikam@aol.com>
2022-01-20 23:14:48 +0100

    commited by software

--- scripts/logfilter.sh
+++ scripts/logfilter.sh
@@ -6,6 +6,15 @@ while [ true ]
 do
   doprint=true
   read line
-  for i in "${misfits_words[@]}"; do echo "$i"; done
+  for i in "${misfits_words[@]}"
+  do
+    echo $i
+    if [[ $line =~ $i ]]
+    then
+      echo true
+    else
+      echo false
+    fi
+  done
   echo $line
 done
